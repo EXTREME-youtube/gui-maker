5637456e2b09fa3a5ee643cb8d7cd05a738014e5
EXTREME-youtube <sterlinghodgeshi@gmail.com>
2025-05-14 08:20:40 -0500

    Update index.js

--- gui maker/src/index.js
+++ gui maker/src/index.js
@@ -1,19 +1,22 @@
-// Import all components
+// Import components
 import { createButton } from "./components/button.js";
 import { makeDraggable } from "./components/draggable.js";
 import { createModal, showModal } from "./components/modal.js";
 import { createTabs } from "./components/tabs.js";
 
-// Export as a UI library object
+// Create a UI library object
 const UI_Library = {
     createButton,
     makeDraggable,
     createModal,
     showModal,
-    createTabs
+    createTabs,
 };
 
-// Allow users to globally access your library
-window.UI_Library = UI_Library;
+// Support both ES module and global browser usage
+if (typeof window !== "undefined") {
+    window.UI_Library = UI_Library;
+}
 
+// Export as default for ES module compatibility
 export default UI_Library;
